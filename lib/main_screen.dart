507f0f743b7e9cda825ad6a2d394cfb0c3be639f
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-04-26 23:26:03 -0700

    impl counter cubit ui

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'counter_cubit/screen.dart';
+
 class MainScreen extends StatelessWidget {
 
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return CounterCubitScreen();
   }
 
 }

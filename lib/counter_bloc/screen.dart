6e514457d58e9b7661dfefd3c6486c9890b6b1d5
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-04-27 00:23:07 -0700

    impl CounterBloc Screen

---
+++ lib/counter_bloc/screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CounterBlocScreen extends StatelessWidget {
+  const CounterBlocScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Cubit'),
+        centerTitle: true,
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.arrow_drop_up_outlined),
+          ),
+          const SizedBox(height: 12),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.arrow_drop_down_sharp),
+          ),
+        ],
+      ),
+      body:  const Center(
+        child: Text('0', style: TextStyle(
+            fontSize: 64, color: CupertinoColors.systemIndigo),),
+      ),
+    );
+  }
+}
